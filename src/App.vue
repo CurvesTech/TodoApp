fff83797d9bad8b519b3c57b795b96bcc694e104
Mubashar Abbas <mubashir.abbas@outlook.com>
2021-07-26 22:29:48 +0500

    todo app fixed up.

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ export default {
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Anton&family=Source+Sans+Pro:wght@300&display=swap');
+  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
   * {
     margin: 0;
     padding: 0;
--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -2,25 +2,47 @@
   <div class="content-box">
     <form @submit.prevent="todoAdded">
       <input 
+        id="inputField"
         type="text" 
         v-model="todo" 
         placeholder="Enter something to do..."
       >
       <button>Add</button>
     </form>
+    <br>
+    <h3>TODO</h3>
+    <hr>
     <ul class="incomplete_list">
-      <li :key="item.id" v-for="(item, index) in incompleteList">
-        <input type="checkbox" v-model="item.completed">
-        {{ index+1 }}. {{ item.name }}
-      </li>
+      <template v-if="incompleteList.length > 0">
+        <li :key="item.id" v-for="(item, index) in incompleteList">
+          <div>
+            <input @change="itemChanged" type="checkbox" v-model="item.completed">
+            {{ index+1 }}. {{ item.name }}  
+          </div>
+          <i @click="removeItem(item.id)" class="color-red fas fa-trash-alt"></i>
+        </li>
+      </template>
+      <template v-else>
+        <div class="p-15 color-red text-center">The list is empty.</div>
+      </template>
     </ul>
     <br>
     <h3>Completed Items</h3>
     <hr>
     <ul class="completed_list">
-      <li :key="item.id" v-for="(item, index) in completedList">
-       {{ index+1 }}. {{ item.name }}
-      </li>
+      <template v-if="completedList.length > 0">
+        <li :key="item.id" v-for="(item, index) in completedList">
+          <div>
+            <input @change="itemChanged" type="checkbox" v-model="item.completed">
+            <i>{{ index+1 }}. {{ item.name }}</i>
+          </div>
+          <i @click="removeItem(item.id)" class="color-red fas fa-trash-alt"></i>
+        </li>
+      </template>
+      <template v-else>
+        <div class="p-15 color-red text-center">The list is empty.</div>
+      </template>
+
     </ul>
   </div>
 </template>
@@ -47,7 +69,28 @@ export default {
       })
     }
   },
+  watch: {
+    todo(newVal) {
+      localStorage.setItem('todo', newVal);
+    }
+  },
+  mounted() {
+    if(localStorage.getItem('todoList') != null) {
+      this.list = JSON.parse(localStorage.getItem('todoList'));
+    }
+    if(localStorage.getItem('todo') != null) {
+      this.todo = localStorage.getItem('todo');
+    }
+  },
   methods: {
+    itemChanged() {
+      localStorage.setItem('todoList', JSON.stringify(this.list));
+    },
+    removeItem(itemId) {
+      this.list = this.list.filter((item) => {
+        return item.id != itemId;
+      });
+    },
     todoAdded() {
       if(this.todo.trim() !== '') {
         this.list.push({
@@ -55,6 +98,12 @@ export default {
           name: this.todo,
           completed: false
         });
+
+        this.todo = '';
+
+        localStorage.setItem('todoList', JSON.stringify(this.list));
+
+        document.getElementById("inputField").focus();
       }
     }
   }
@@ -62,9 +111,20 @@ export default {
 </script>
 
 <style scoped>
+  .p-15 {
+    padding: 15px;
+  }
+  .color-orange {
+    color: orange;
+  } 
+  .color-red {
+    color: red;
+  } 
+  .text-center {
+    text-align: center;
+  }
   .content-box form {
     display: flex;
-
   }
   .content-box > form > input {
     font-size: 24px;
@@ -91,17 +151,41 @@ export default {
     flex: 1;
   }
 
+  input[type="checkbox"] {
+    margin-right: 20px;
+  }
+
   .content-box {
     min-height: 70vh;
-    padding: 10px;
+    padding: 20px;
   }
 
   .content-box h3 {
     font-size: 22px;
   }
-  .completed_list li {
+
+  .incomplete_list li {
+    font-size: 18px;
+    font-weight: bold;
+    color: rgb(17, 17, 173);
+    padding: 10px;
+  }
+  ul li {
+    display: flex;
+    justify-content: space-between;
+  }
+  .completed_list > li > div > i {
     text-decoration: line-through;
+  }
+
+  .completed_list li {
     color: grey;
+    padding: 10px;
+    font-size: 18px;
+  }
+
+  ul {
+    list-style-type: none;
   }
 
 </style>
